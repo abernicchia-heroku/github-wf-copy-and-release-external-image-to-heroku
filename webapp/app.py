f2cf620fb2c53e078b0fefa6367725562a151a66
Andrea Bernicchia <abernicchia@salesforce.com>
2025-04-08 10:35:18 +0200

    text msg updated

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world! 20250408'
+    return 'Hello world! 20250408 10:34'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
